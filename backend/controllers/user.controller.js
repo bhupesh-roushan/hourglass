5eccdb83dab5846058e01364a72c5f6fa4547a7d
bhupesh-roushan <roushan.bhupesh@gmail.com>
2025-01-22 23:57:16 +0530

    error solved

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import getDataUri from "../utils/datauri.js";
+import getDataUri from "../utils/dataUri.js";
 import cloudinary from "../utils/cloudinary.js";
 import { Post } from "../models/post.model.js";
 export const register = async (req, res) => {
