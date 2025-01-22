b7add4d5512f8d322d04993869dab55debc3b4f5
bhupesh-roushan <roushan.bhupesh@gmail.com>
2025-01-23 00:14:12 +0530

    error solved

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -49,7 +49,7 @@ const EditProfile = () => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:8000/api/v1/user/profile/edit",
+        "https://hourglass-0e3w.onrender.com/api/v1/user/profile/edit",
         formData,
         {
           headers: {
--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -32,7 +32,7 @@ const LeftSidebar = () => {
 
   const logoutHandler = async () => {
     try {
-      const res = await axios.get("http://localhost:8000/api/v1/user/logout", {
+      const res = await axios.get("https://hourglass-0e3w.onrender.com/api/v1/user/logout", {
         withCredentials: true,
       });
       if (res.data.success) {
